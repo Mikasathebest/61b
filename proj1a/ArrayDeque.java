bacd746bc2687f1a0fc28d841e08fd867beea1c1
Mikasathebest <djf2017116@gmail.com>
2022-05-02 00:11:41 +0800

    update Array Deque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,37 +1,104 @@
-public class ArrayDeque <Item>{
-    private Item[] items;
+// Create a circular ArrayDeque
+public class ArrayDeque <T>{
+    private T[] items;
     private int size;
+    private int nextFirst;
+    private int nextLast;
 
-    /** Create an empty Array Deque*/
-    public void ArrayDeque(){
-        items = (Item[]) new Object[100];
+    private int refactor = 2;
+
+    /** Create an empty Array Deque.
+     *  This Array is not traditional array.
+     *  It uses two pointers nextFirst, nextLast to store the start point and the end point.
+     * */
+    public  ArrayDeque(){
+        items = (T[]) new Object[8];
+        // random initialize
+        nextFirst = 5;
+        nextLast = nextFirst + 1;
         size = 0;
     }
 
-    /** Adds an item of type Item to the front of the deque.*/
-    public void addFirst(Item item){
-        Item[] p = (Item[]) new Object[size+1];
-        p[0] = item;
-        System.arraycopy(item, 0, p, 1, size);
-        items = p;
-        size += 1;
-    }
+    /** Private helper.
+     * Resize the array when encounter below cases:
+     * 1. Array is full:
+     *  Create a new array: {null, 1, 2, 3, null, null}
+     *  nextFirst points to 0; nextLast points to 4 --- size + 1.
+     * 2. nextFirst or nextLast point needs to update.
+     *      a. nextFirst points to the tail of the array. Expand the array and refresh it.
+     *      b. nextLast points to the
+     * */
+    private void resize(int resizer) {
+        T[] p = (T[]) new Object[resizer];
+        int start = addOne(nextFirst);
+        int end = minusOne(nextLast);
 
-    private void resize(int a){
-        Item[] p = (Item[]) new Object[a];
-        System.arraycopy(items, 0, p, 0, size);
-        items = p;
-    }
-
-    /** Adds an item of type Item to the back of the deque.*/
-    public void addLast(Item item){
-        int Refactor = 2;
-        if(size == items.length){
-            resize(size * Refactor);
+        // Expand the array
+        if (nextFirst == end) {
+            System.arraycopy(items, start, p, 1, items.length - start);
+            System.arraycopy(items, 0, p, items.length - start + 1, start);
+            nextFirst = 0;
+            nextLast = size + 1;
+            items = p;
         }
-        items[size] = item;
+        else{
+            // Reduce the array to save memory. When usage ratio < 0.25.
+            if (nextFirst < nextLast) {
+                System.arraycopy(items, start, p, 1, size);
+            }
+            else { // nextFirst > nextLast
+                System.arraycopy(items, start, p, 1, items.length - start);
+                System.arraycopy(items, 0, p, items.length - start + 1, size - items.length + start);
+
+            }
+            items = p;
+            nextFirst = 0;
+            nextLast = size + 1;
+        }
+    }
+
+    /** Private helper function to move pointer.*/
+    private int addOne(int index){
+        if (index == items.length -1){
+            index = 0;
+        }
+        else{
+            index += 1;
+        }
+        return index;
+    }
+
+    /** Private helper function to minus pointer. */
+    private int minusOne(int index){
+        if (index == 0){
+            index = items.length - 1;
+        }
+        else{
+            index -= 1;
+        }
+        return index;
+    }
+
+    /** Adds an item of type int to the front of the deque.*/
+    public void addFirst(T item) {
+        if (size == items.length){
+            resize(size * refactor);
+        }
+        items[nextFirst] = item;
+        nextFirst = minusOne(nextFirst);
         size += 1;
     }
+
+    /** Adds an item of type int to the back of the deque.*/
+    public void addLast(T item){
+        if(size == items.length){
+            resize(size * refactor);
+        }
+        items[nextLast] = item;
+        nextLast = addOne(nextLast);
+        size += 1;
+    }
+
 
     /** Returns true if deque is empty, false otherwise. */
     public boolean isEmpty(){
@@ -48,20 +115,30 @@ public class ArrayDeque <Item>{
 
     /** Prints the items in the deque from first to last, separated by a space.*/
     public void printDeque(){
-        int i;
-        for (i=0;i<size; i++){
-            System.out.print(items[i] + " ");
+        if (size == 0){
+            System.out.println("Empty array!");
+        }
+        else{
+            int i = addOne(nextFirst);
+            for(int j = 0; j < size; j++){
+                System.out.print(items[i] + " ");
+                i = addOne(i);
+            }
         }
     }
 
     /** Removes and returns the item at the front of the deque. If no such item exists, returns null.*/
-    public Item removeFirst(){
+    public T removeFirst(){
         if (size >= 1){
-            Item res = items[0];
-            Item[] p = (Item[]) new Object[size-1];
-            System.arraycopy(items, 1, p, 0,size-1);
-            items = p;
+            T res = items[addOne(nextFirst)];
+            items[addOne(nextFirst)] = null;
+            nextFirst = addOne(nextFirst);
             size -= 1;
+
+            // Resize the array to save memory.
+            if (items.length > 16 && size < items.length / 4){
+                resize(size / refactor);
+            }
             return res;
         }
         else{
@@ -71,13 +148,16 @@ public class ArrayDeque <Item>{
 
     /** Removes and returns the item at the back of the deque.
      * If no such item exists, returns null.*/
-    public Item removeLast(){
+    public T removeLast(){
         if (size >= 1){
-            Item res = items[size - 1];
-            Item[] p = (Item[]) new Object[size-1];
-            System.arraycopy(items, 0, p, 0,size-1);
-            items = p;
+            T res = items[minusOne(nextLast)];
+            items[minusOne(nextLast)] = null;
+            nextLast = minusOne(nextLast);
             size -= 1;
+
+            if (items.length > 16 && size < items.length / 4){
+                resize(size / refactor);
+            }
             return res;
         }
         else{
@@ -88,12 +168,34 @@ public class ArrayDeque <Item>{
     /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
      *  If no such item exists, returns null. Must not alter the deque!
      */
-    public Item get(int index){
-        if (index <= size-1){
-            return items[index];
-        }
-        else{
+    public T get(int index){
+        if (index > size -1 || index < 0){
             return null;
         }
+        else{
+            index = (index + addOne(nextFirst)) % items.length;
+            return items[index];
+        }
     }
+
+//    public static void main(String[] args){
+//        ArrayDeque1<Integer> ll1 = new ArrayDeque1<>();
+//        ll1.addFirst(4);
+//        //ll1.printDeque();
+//        ll1.addFirst(3);
+//        ll1.addFirst(2);
+//        ll1.addFirst(1);
+//        ll1.addLast(5);
+//        //ll1.printDeque();
+//        ll1.addFirst(0);
+//        ll1.addLast(6);
+//        ll1.addLast(7);
+//        ll1.addLast(8);
+//        ll1.addLast(9);
+//        ll1.addLast(10);
+//        ll1.printDeque();
+//
+//        System.out.println(ll1.get(5));
+//
+//    }
 }
