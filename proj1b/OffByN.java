426460e1424f6b3319ddf90db3a0e8acbdce89b8
harry <djf2017116@gmail.com>
2022-05-08 22:37:47 +0800

    pass autograder

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -4,31 +4,8 @@ public class OffByN implements CharacterComparator {
 
     @Override
     public boolean equalChars(char x, char y) {
-
-        if ((inUpperAlphabet(x) && inUpperAlphabet(y)) ||
-            (inLowerAlphabet(x) && inLowerAlphabet(y))) {
-            int subtract = Math.abs(x - y);
-            return (subtract == N);
-        } else {
-            return false;
-        }
-    }
-
-    /** Judge if a given char in A-Z*/
-    private boolean inUpperAlphabet(char c) {
-        if (64 < c && c < 91) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    /** Judge if a given char in a-z*/
-    private boolean inLowerAlphabet(char c) {
-        if (96 < c && c < 123) {
-            return true;
-        } else {
-            return false;
-        }
+        int subtract = Math.abs(x - y);
+        return (subtract == N);
     }
 
     public OffByN(int n) {
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -5,12 +5,7 @@ public class OffByOne implements CharacterComparator {
      * such that equalChars returns true for characters that are different by exactly one. */
     @Override
     public boolean equalChars(char x, char y) {
-        if ((64 < x && x < 91) || (96 < x && x < 122)) {
-            if ((64 < y && y < 91) || (96 < y && y < 122)) {
-                int subtract = Math.abs(x - y);
-                return (subtract == 1);
-            }
-        }
-        return false;
+        int subtract = Math.abs(x - y);
+        return (subtract == 1);
     }
 }
