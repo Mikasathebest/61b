96b7d32e706521485be47ce24b9c0b8b14b8b40b
harry <djf2017116@gmail.com>
2022-05-08 13:56:47 +0800

    fix bug and add for loop for remove action

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -19,19 +19,22 @@ public class TestArrayDequeGold {
                 msgAddOrRemove += String.format("addLast(%d)\n", i);
             } else {
                 sad1.addFirst(i);
-                ads1.addLast(i);
+                ads1.addFirst(i);
                 msgAddOrRemove += String.format("addFirst(%d)\n", i);
             }
         }
 
 //        assertEquals(String.format("You give size %d. It should be %d",
 //                sad1.size(), ads1.size()), sad1.size() , ads1.size());
-
-        msgAddOrRemove += String.format("removeFirst()\n");
-        assertEquals(msgAddOrRemove, sad1.removeFirst(), ads1.removeFirst());
-
-        msgAddOrRemove += String.format("removeLast()\n");
-        assertEquals(msgAddOrRemove, sad1.removeLast(), ads1.removeLast());
-
+        for (int i = 0; i < 9; i++) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            if (numberBetweenZeroAndOne < 0.5) {
+                msgAddOrRemove += String.format("removeFirst()\n");
+                assertEquals(msgAddOrRemove, ads1.removeFirst(), sad1.removeFirst());
+            } else {
+                msgAddOrRemove += String.format("removeLast()\n");
+                assertEquals(msgAddOrRemove, ads1.removeLast(), sad1.removeLast());
+            }
+        }
     }
 }
